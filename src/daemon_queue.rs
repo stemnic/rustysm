b3b4f356591b981d87b1665b80d648d135c5ae4d
Ole Sivert Aarhaug <ole.sivert@hotmail.com>
2021-02-28 15:43:02 +0100

    Added some more notes and tinkered some more

--- src/daemon_queue.rs
+++ src/daemon_queue.rs
@@ -6,14 +6,16 @@ use crate::daemon_downloader::*;
 
 #[derive(Debug)]
 pub struct DaemonQueue{
-    queue: Vec<QueueObject>
+    media_queue: Vec<QueueObject>,
+    to_be_processed_queue: Vec<PreQueueObject>
 }
 
 #[derive(Debug)]
 struct QueueObject {
     id: u64,
     priority: u64,
-    source: String, // Filepath, url, whater
+    path: String, // Filepath, url, whater
+    title: String,
     object_type: ObjectTypes,
     playback_postition: f64,
     settings: Settings
@@ -35,21 +37,37 @@ struct Settings {
     // Raw set properties for mpv vec with a str and template element?
 }
 
+#[derive(Debug)]
 /// Input proccessed before adding to media queue
-struct InputObject {
+pub struct InputObject {
     input_string: String,
     priority: u64,
     settings: Settings
+}
+
+#[derive(Debug)]
+struct PreQueueObject {
+    input: InputObject,
+    finished: bool,
 }
 
 /* 
 1. Input object recived
 2. Determines if it's a local file or downloadable file based on input
 3. Add result to media queue
+Alternaitve
+1. Get file to play, start downloading it and wait with mpv playing until the download daemon have started.
 */
 
 impl DaemonQueue {
     pub fn new() -> Result<Self, io::Error> {
-        Ok(DaemonQueue{ queue: vec![] })
+        Ok(DaemonQueue{ media_queue: vec![], to_be_processed_queue: vec![] })
+    }
+    pub fn add_to_queue(&mut self, object: InputObject ) {
+        // When you attempt to add a object to the queue it needs to be processed first
+        self.to_be_processed_queue.push( PreQueueObject{ input: object, finished: false } );
+    }
+    pub fn process_prequeue(&mut self) {
+        
     }
 }
